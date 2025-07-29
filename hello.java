e7b3b5015582e0ac0b644d4efd4e99ce7c857005
olivier010 <oliviernsengiyumva010@gmail.com>
2025-07-29 06:13:05 +0300

    initial commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        //addition of two numbers
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("the sum of two number is" + sum);
+    
+    }
+}
